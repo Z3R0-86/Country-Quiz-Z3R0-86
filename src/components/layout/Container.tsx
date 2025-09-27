6234844ad22900fa2147115d0b8a642083c630d9
Z3R0-86 <k86029@gmail.com>
2025-09-27 04:36:35 -0600

    mejoras en la forma de ver en dispositivos

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -2,24 +2,31 @@ import styled from 'styled-components';
 import { theme } from '../../styles/theme';
 
 export const Container = styled.div`
-  width: 60%;
+  width: 92%;
   max-width: 850px;
   margin: 0 auto;
-  padding: 0 ${theme.spacing.md};
+  padding: 0 ${theme.spacing.sm};
+
+  @media (min-width: ${theme.breakpoints.mobile}) {
+    width: 60%;
+    padding: 0 ${theme.spacing.md};
+  }
 `;
 
 export const QuizContainer = styled.div`
   background-color: ${theme.colors.background.main};
-  padding: ${theme.spacing.xxl} ${theme.spacing.xl};
+  padding: ${theme.spacing.lg} ${theme.spacing.md};
   border-radius: ${theme.borderRadius.medium};
-  margin-top: 50px;
+  margin-top: 30px;
   display: flex;
   flex-direction: column;
-  gap: ${theme.spacing.xl};
+  gap: ${theme.spacing.lg};
   box-shadow: ${theme.shadows.large};
 
   @media (min-width: ${theme.breakpoints.mobile}) {
     padding: ${theme.spacing.xxl} ${theme.spacing.xxl};
+    margin-top: 50px;
+    gap: ${theme.spacing.xl};
   }
 `;
 
@@ -27,6 +34,10 @@ export const QuizCompleteWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 48px;
+  margin-top: 24px;
   width: 100%;
+
+  @media (min-width: ${theme.breakpoints.mobile}) {
+    margin-top: 48px;
+  }
 `;
